843879c2d08f4f1ffac28b9243bb283a2d2979f7
numabesachi <numabesachi@gmail.com>
2020-09-20 00:16:51 +0900

    finish

---
+++ app/src/main/java/com/example/l4s_skill_edittext/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.l4s_skill_edittext
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        button.setOnClickListener {
+//            if(editText.text != null){
+//                textView.text = editText.text.toString()
+//            }else{
+//                textView.text = "何か記入してね！"
+//            }
+//        }
+
+        button.setOnClickListener {
+            if(editText.length() != 0){
+                textView.text = editText.text.toString()
+            }else{
+                textView.text = "何か記入してね！"
+            }
+        }
+    }
+
+}
